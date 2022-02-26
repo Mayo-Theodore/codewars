56bd559aec997b7a676bdcacf16af77fae9df470
Mayo Theodore <mayoelith@gmail.com>
2022-02-26 11:25:25 +0000

    divisors kata

---
+++ 7-find_divisors/find_divisors.py
@@ -0,0 +1,33 @@
+class Divisors():
+    def is_greater(self, n):
+        if n <= 1:
+            return "Integer must be greater than 1"
+        else:
+            return "Integer is greater than 1"
+
+    def divisible_by(self, n):
+        array = []
+        i = 1000
+        while i >= 2:
+            if n == i:
+                pass
+            elif n % i == 0:
+                array.append(int(n / i))
+            i -= 1
+
+        return array
+
+    def is_prime(self, n):
+        array = []
+        i = 1000
+        while i >= 2:
+            if n == i:
+                pass
+            elif n % i == 0:
+                array.append(int(n / i))
+            i -= 1
+
+        if not array:
+            return f"{n} is prime"
+        
+
---
+++ 7-find_divisors/test_find_divisors.py
@@ -0,0 +1,25 @@
+import pytest
+from find_divisors import *
+
+def test_integer_greater_than_1():
+    divisors = Divisors()
+    assert divisors.is_greater(1) == "Integer must be greater than 1"
+    assert divisors.is_greater(3) == "Integer is greater than 1"
+
+def test_divisible_by():
+    divisors = Divisors()
+    assert divisors.divisible_by(15) == [3, 5]
+    assert divisors.divisible_by(24) == [2,3,4,6,8,12]
+    assert divisors.divisible_by(25) == [5]
+    assert divisors.divisible_by(253) == [11, 23]
+
+def test_is_prime():
+    divisors = Divisors()
+    assert divisors.is_prime(3) == "3 is prime"
+    assert divisors.is_prime(17) == "17 is prime"
+    assert divisors.is_prime(29) == "29 is prime"
+
+
+
+    
+
